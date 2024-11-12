1fb1ab6a421e79ec136416fe3f8fd012510fb5e1
DaveLee-b <lpoln1855@gmail.com>
2024-11-12 11:41:48 +0900

    test1

--- src/main/java/gcukakao/jenkins/calculator/Calculator.java
+++ src/main/java/gcukakao/jenkins/calculator/Calculator.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
      public int sum(int a, int b) {
+          System.out.println("201935091 이동석");
           return a + b;
      }
      public int sub(int a, int b) {
